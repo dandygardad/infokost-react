5ef4b870c07e639dc55f9c6a1d9553a8d4546905
dandygardad <dandygardad@gmail.com>
2022-10-31 15:33:20 +0800

    Add Lazy Load to `FlatCard`

--- src/components/global/FlatCard.jsx
+++ src/components/global/FlatCard.jsx
@@ -1,4 +1,5 @@
 import { HashLink as Link } from "react-router-hash-link"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 
 /** @jsxImportSource @emotion/react */
 import * as style from './FlatCard.style'
@@ -8,7 +9,9 @@ const urlPicture = `${process.env.REACT_APP_API_URL}/uploads/`
 const FlatCard = (props) => {
     return(
         <div role="listitem" css={style.container}>
-            <img src={urlPicture.concat(props.image)} alt={"Gambar dari " + props.name} css={style.cardImage} />
+            {/* <img src={urlPicture.concat(props.image)} alt={"Gambar dari " + props.name} css={style.cardImage} /> */}
+            {/* <img src={props.image} alt={"Gambar dari " + props.name} css={style.cardImage} /> */}
+            <LazyLoadImage css={style.cardImage} alt={"Gambar dari " + props.name} src={urlPicture.concat(props.image)} effect={"blur"} placeholderSrc={require('../../assets/img/empty.webp')} />
             {/* <img src={require('../../assets/img/' + props.image)} alt={"Gambar dari " + props.name} css={style.cardImage} /> */}
             <div css={style.containerCard}>
                 <h4>{props.name}</h4>
